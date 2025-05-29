3cb12f4d752d51bb258a007f9e547e57d571d67b
henriquez <henriquez@solintels.com>
2025-05-29 14:35:34 -0400

    config db1

--- sistema/settings.py
+++ sistema/settings.py
@@ -114,14 +114,15 @@ WEASYPRINT_SETTINGS = {
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
 'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'tienda',
+        'NAME': 'railway',
         'USER': 'root',
-        'PASSWORD': '',
-        'HOST':'localhost',
-        'PORT': '3306',
+        'PASSWORD': 'NqBmlOwcypFuPkbuOFILEluQVhjyweCQ',
+        'HOST':'hopper.proxy.rlwy.net',
+        'PORT': '57454',
     }
 }
 
